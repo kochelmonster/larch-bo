74b100a5367c3e3c7ce88adeba73dbe7540fe9fb
kochelmonster <mreithinger@web.de>
2021-12-28 00:43:34 +0100

    polymer controls.

---
+++ bo/client/larch/bo/client/polymer/__init__.py
@@ -0,0 +1,5 @@
+from .paper_input import PaperInput
+
+# __pragma__("skip")
+__all__ = ("PaperInput",)
+# __pragma__("noskip")
---
+++ bo/client/larch/bo/client/polymer/paper_input.py
@@ -0,0 +1,30 @@
+from larch.reactive import rule, Cell
+from larch.bo.client.control import Control, register
+
+# __pragma__("skip")
+from larch.bo.packer import parcel
+document = None
+parcel.NEEDED_PACKAGES.append("@polymer/paper-input")
+def require(p): pass
+# __pragma__("noskip")
+
+
+require("@polymer/paper-input/paper-input.js")
+
+
+@register(type(""), "polymer")
+class PaperInput(Control):
+    element = Cell()
+
+    def render(self, parent):
+        self.element = document.createElement("paper-input")
+        parent.appendChild(self.element)
+        self.element.addEventListener("change", self.on_change)
+
+    def on_change(self, event):
+        self.context.value = self.element.value
+
+    @rule
+    def _rule_value_changed(self):
+        if self.element:
+            self.element.setAttribute("value", self.context.value)
